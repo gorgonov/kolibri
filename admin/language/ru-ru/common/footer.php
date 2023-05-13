eb0d5d77cd4d65a62c24d22cf697ea16566fa720
gorgonov <aleksander.gorgonov@yandex.ru>
2023-05-13 15:44:01 +0300

    synchronize version opencart

--- admin/language/ru-ru/common/footer.php
+++ admin/language/ru-ru/common/footer.php
@@ -1,5 +1,5 @@
 <?php
 // Text
-$_['text_footer']  = '<HR><a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Все права защищены.<br> <BR> <a href="http://opencart-russia.ru" target="_blank">Русская сборка OpenCart</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="http://forum.opencart-russia.ru" target="_blank">Форум поддержки</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="http://shop.opencart-russia.ru" target="_blank">Магазин дополнений</a>';
-$_['text_version'] = 'Version %s (rs.3)';
+$_['text_footer'] 	= '<hr><a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Все права защищены.<br> <a href="https://opencart3x.ru" target="_blank">Модули для OpenCart 3</a>';
+$_['text_version'] 	= 'Version %s';
 
